1778f4e60f776e2381e8e81118f0d4de2bbb1a79
DataScienceHKU <60804044+DataScienceHKU@users.noreply.github.com>
2020-02-09 21:29:55 +0800

    Add files via upload

---
+++ Final_Project.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# # Introduction 
+# Toronto is the provincial capital of Ontario and the most populous city in Canada, with a population of more than 2.7 Million. It is an international centre of business, finance, arts, and culture, and is recognized as one of the most multicultural and cosmopolitan cities in the world.
+# 
+# Toronto is remarkable in diversity in its food and restaurants. Pakistani, Persian, Portuguese; aboriginal and new fusion; Japanese pancakes and Korean barbecue; fresh pasta in Little Italy, shawarmas in Greektown and the best damn dumplings in Chinatown. Torontonians love to eat out, whether it's sitting at sidewalk bistros on a warm summer night or getting all bundled up for some hot Vietnamese pho.
+# 
+# So, I would love to explore a new investment and business in food. If we think of both city residents and travelers, they may want to choose different types of food. At the same time, they may want to choose local, district or signature Toronto's dishes.
+# 
+# In our business problem, we will perform analysis to understand the current market and decide which market and types of food that we will invest, and create a map with clusters to reveal which areas we should consider to start our business.
+# 
+# # Data
+# 1) Table of postal codes in Toronto will be used to identify the Postal Code, Borough and Neighborhood.
+# 2) Geospatial Data of Canada will be used in order to get the latitude and longitude coordinates of a given postal code in Canada as well as Toronto.
+# 3) Foursquare API data will be used to explore venues around Toronto, the top 5 venues around these areas, explore users' tips, and finally perform clustering.
